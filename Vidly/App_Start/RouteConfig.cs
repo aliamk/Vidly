641c5add7fd8a2d68d0d687a8fa94fe7e09d7306
ALIA <ALIA@DESKTOP-HA40DSC>
2020-10-19 14:05:54 +0100

    added customer and movie models and converted the Navbar into a partil view

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -13,7 +13,22 @@ namespace Vidly
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
+            //ATTRIBUTE ROUTING - NEW WAY TO ADD CUSTOM ROUTES - configure in Controller + Random.cshtml
+            routes.MapMvcAttributeRoutes();
+
+            // OLD WAY TO MAKE CUSTOM ROUTES - REPLACED BY ATTRIBUTE ROUTING: Custom Route (must position BEFORE/ABOVE the default route & remember to create the Action in Controller)
+            /* routes.MapRoute(
+                "MoviesByReleaseDate",
+                "movies/released/{year}/{month}",
+                new { controller = "Movies", action = "ByReleaseDate"},
+                // Add constraints: verbatim string using @ because of the slash, digit is represented by d, repetitions repped by 4:  2015/04
+                new { year = @"\d{4}", month = @"\d{2}"}
+                // new { year = @"2015¦2016", month = @"\d{2}" }  movie list is contrained to the years 2015-2016
+                );
+            */
+
+            // Default Route
+             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,15 +1,76 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET
-        public ActionResult Random()
+        // VIEW ONE:   GET: https://localhost:44354/movies/Random
+        public object Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
-            return View(movie);
+
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"}
+            };
+
+            // Initialise the Move and Customers properties
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            // Method 1:  Pass data from Controller to View using a ViewDataDictionary  - NOT PREFERABLE WAY
+            // ViewData["Movie"] = movie; // using this method, remove movie from return View(movie) below
+
+            // Method 2: Pass data from Controller to View using a ViewBag   - NOT PREFERABLE WAY
+            // ViewBag.Movie = movie;
+
+            // Method 3:  Pass data from Controller to View using a ViewData - preferable way
+            //var ViewResult = new ViewResult();
+            //_ = ViewResult.ViewData.Model;
+
+            
+
+            return View(viewModel);
+            // return Content("Hello World!");
+            // return HttpNotFound();
+            // return new EmptyResult();
+            // return RedirectToAction("Index", "Home", new {page = 1, sortBy = "name"});
+        }
+
+        // VIEW CUSTOM ROUTE:  https://localhost:44354/movies/released/2015/4 (make sure this is configured in RouteConfig)
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]  // Applying attribute routing as setup in RouteConfig - month must be two digits and between 1 - 12
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        // VIEW TWO:   GET:  https://localhost:44354/movies/edit/1
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+
+        // VIEW THREE
+        // GET:  https://localhost:44354/movies  RETURN A VIEW CONTAINING A LIST OF MOVIES SPANNING MULTIPLE WEBPAGES. OPTIONAL PARAMETERS: PAGE INDEX AND SORTBY 
+        public ActionResult Index(int? pageIndex, string sortBy) // THESE SHOULD BE NULLIBLE... THE QUESTION MARK CONVERTS THE INT TO NULLIBLE
+        {
+            if (!pageIndex.HasValue) // IF PAGE IS NOT SPECIFIED BY USER, BY DEFAULT SHOW PAGE 1
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy)) //  IF SORT METHOD IS NOT SELECTED BY USER SHOW MOVIES IN ALPHABETICAL ORDER
+                sortBy = "Name";
+
+            // 
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
     }
 }
